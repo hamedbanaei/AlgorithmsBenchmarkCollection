d931d677fdd025fca4aa1f59eb4d161cbee6884e
Hamed Banaei <hamedbanaei1982@gmail.com>
2023-09-29 02:48:33 +0330

    Revert "Revert "ForeachBenchmark Done.""
    This reverts commit c3a772e9f258f1e63361e99b4346be813b46524c.

---
+++ GeneralAlgorithmsBenchmarks/Person.cs
@@ -0,0 +1,8 @@
+﻿namespace GeneralAlgorithmsBenchmarks;
+
+public class Person: System.Object
+{
+    public int Age { get; set; }
+
+    public string? Name { get; set; }
+}
--- GeneralAlgorithmsBenchmarks/Program.cs
+++ GeneralAlgorithmsBenchmarks/Program.cs
@@ -1,1 +1,2 @@
-﻿var summary = BenchmarkDotNet.Running.BenchmarkRunner.Run<GeneralAlgorithmsBenchmarks.WritingFileBenchmark>();
+﻿//var summary = BenchmarkDotNet.Running.BenchmarkRunner.Run<GeneralAlgorithmsBenchmarks.WritingFileBenchmark>();
+var summary = BenchmarkDotNet.Running.BenchmarkRunner.Run<GeneralAlgorithmsBenchmarks.ForeachBenchmark>();
