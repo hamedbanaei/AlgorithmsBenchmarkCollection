16eb2f8e8903292456166f7add31210ec90ab94f
Hamed Banaei <hamedbanaei1982@gmail.com>
2023-10-02 22:11:26 +0330

    some changes due to WhereAndSelectPriority Benchmark done in program.cs

--- EntityFrameworkCoreBenchmark/Program.cs
+++ EntityFrameworkCoreBenchmark/Program.cs
@@ -1,2 +1,3 @@
 ﻿var summary = BenchmarkDotNet.Running.BenchmarkRunner.Run<Benchmarks.TraditionalUpdateVsExecuteUpdate>();
 //var summary = BenchmarkDotNet.Running.BenchmarkRunner.Run<Benchmarks.TraditionalDeleteVsExecuteDelete>();
+//var summary = BenchmarkDotNet.Running.BenchmarkRunner.Run<Benchmarks.WhereAndSelectPriority>();
