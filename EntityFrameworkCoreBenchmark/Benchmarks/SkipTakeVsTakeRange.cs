fc209982987105fbcf068b98a480a01a615ef2a4
Hamed Banaei <hamedbanaei1982@gmail.com>
2023-10-03 21:09:14 +0330

    SkipTakeVsTakeRange Benchmark designed.

---
+++ EntityFrameworkCoreBenchmark/Benchmarks/SkipTakeVsTakeRange.cs
@@ -0,0 +1,35 @@
+﻿namespace Benchmarks;
+
+[BenchmarkDotNet.Attributes.MemoryDiagnoser]
+[BenchmarkDotNet.Attributes.GroupBenchmarksBy(BenchmarkDotNet.Configs.BenchmarkLogicalGroupRule.ByParams)]
+public class SkipTakeVsTakeRange
+{
+    [BenchmarkDotNet.Attributes.Params
+        (1_000, 2_000, 5_000, 10_000, 20_000, 50_000, 100_000, 200_000, 500_000, 1_000_000)]
+    public int Rows { get; set; }
+
+    [BenchmarkDotNet.Attributes.GlobalSetup]
+    public void GlobalSetup()
+    {
+        Benchmarks.GlobalTestSetup.FillDatabaseWithFakeTestData
+            (testFatherCount: 2_000_000, ignoreCheckIfDatabaseHasData: false);
+    }
+
+    [BenchmarkDotNet.Attributes.Benchmark]
+    public void SkipTake()
+    {
+        using var dbContext = new Model.DatabaseContext();
+
+        var recordsToUpdate = dbContext.Children.Skip(Rows).Take(Rows).ToList();
+
+        dbContext.SaveChanges();
+    }
+
+    [BenchmarkDotNet.Attributes.Benchmark]
+    public void TakeRange()
+    {
+        using var dbContext = new Model.DatabaseContext();
+
+        dbContext.Fathers.Take(Rows .. Rows).ToList();
+    }
+}
--- EntityFrameworkCoreBenchmark/Program.cs
+++ EntityFrameworkCoreBenchmark/Program.cs
@@ -1,3 +1,4 @@
-﻿var summary = BenchmarkDotNet.Running.BenchmarkRunner.Run<Benchmarks.TraditionalUpdateVsExecuteUpdate>();
-//var summary = BenchmarkDotNet.Running.BenchmarkRunner.Run<Benchmarks.TraditionalDeleteVsExecuteDelete>();
+﻿var summary = BenchmarkDotNet.Running.BenchmarkRunner.Run<Benchmarks.SkipTakeVsTakeRange>();
 //var summary = BenchmarkDotNet.Running.BenchmarkRunner.Run<Benchmarks.WhereAndSelectPriority>();
+//var summary = BenchmarkDotNet.Running.BenchmarkRunner.Run<Benchmarks.TraditionalUpdateVsExecuteUpdate>();
+//var summary = BenchmarkDotNet.Running.BenchmarkRunner.Run<Benchmarks.TraditionalDeleteVsExecuteDelete>();
