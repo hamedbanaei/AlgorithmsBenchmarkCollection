8091d3f26ed9397dd61b18d6e40ff62e98ed1a13
Hamed Banaei <hamedbanaei1982@gmail.com>
2023-10-06 15:14:19 +0330

    Some changes.

--- EntityFrameworkCoreBenchmark/Benchmarks/Linq/ImprovePerformanceWithAsNoTracking.cs
+++ EntityFrameworkCoreBenchmark/Benchmarks/Linq/ImprovePerformanceWithAsNoTracking.cs
@@ -9,7 +9,7 @@ namespace Benchmarks.Linq;
 public class ImprovePerformanceWithAsNoTracking
 {
     [BenchmarkDotNet.Attributes.Params
-        (1_000, 2_000, 5_000, 10_000)]
+        (1_000, 2_000, 5_000, 10_000, 20_000, 50_000, 100_000, 200_000, 500_000, 1_000_000)]
     public int Rows { get; set; }
 
     [BenchmarkDotNet.Attributes.GlobalSetup]
@@ -30,7 +30,7 @@ public class ImprovePerformanceWithAsNoTracking
     public List<Model.Child> FetchDataWithAsNoTracking()
     {
         using var dbContext = new Model.DatabaseContext();
-        return dbContext.Children.AsNoTracking().Take(Rows).ToList();
+        return dbContext.Children.Take(Rows).AsNoTracking().ToList();
     }
 
     //public static IEnumerable<Model.Father> ReturnNullRealJob()
