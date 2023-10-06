71b1c422705d4f4ad770522047c0a6c3380de681
Hamed Banaei <hamedbanaei1982@gmail.com>
2023-10-06 15:55:42 +0330

    Some minor changes.

--- EntityFrameworkCoreBenchmark/Benchmarks/Linq/ImprovePerformanceWithAsNoTracking.cs
+++ EntityFrameworkCoreBenchmark/Benchmarks/Linq/ImprovePerformanceWithAsNoTracking.cs
@@ -16,7 +16,7 @@ public class ImprovePerformanceWithAsNoTracking
     public void GlobalSetup()
     {
         Benchmarks.GlobalAndIterationSetup.FillDatabaseWithFakeTestData
-            (testFatherCount: 10_000, ignoreCheckIfDatabaseHasData: false);
+            (testFatherCount: 2_000_000, ignoreCheckIfDatabaseHasData: false);
     }
 
     [BenchmarkDotNet.Attributes.Benchmark]
@@ -32,28 +32,4 @@ public class ImprovePerformanceWithAsNoTracking
         using var dbContext = new Model.DatabaseContext();
         return dbContext.Children.Take(Rows).AsNoTracking().ToList();
     }
-
-    //public static IEnumerable<Model.Father> ReturnNullRealJob()
-    //{
-    //    IEnumerable<Model.Father> result;
-
-    //    using var DbContext = new Model.DatabaseContext();
-    //    result = DbContext.Fathers.Where(a => a.Name == "Hamed Banaei" && a.NickName == "MrTechLead");
-
-    //    return (result);
-    //}
-
-    //public static IEnumerable<Model.Father> ReturnEmptyEnumerableRealJob()
-    //{
-    //    IEnumerable<Model.Father> result;
-
-    //    using var DbContext = new Model.DatabaseContext();
-    //    result = DbContext.Fathers.Where(a => a.Name == "Hamed Banaei" && a.NickName == "MrTechLead");
-
-    //    if (result == null)
-    //    {
-    //        result = Enumerable.Empty<Model.Father>();
-    //    }
-    //    return (result);
-    //}
 }
