c2e780018f01ec31c97c13730d561307eac627c0
Hamed Banaei <hamedbanaei1982@gmail.com>
2023-10-08 22:20:48 +0330

    Added MrTechLead project with some minor visualization effects on console.

---
+++ MrTechLead/Console.cs
@@ -0,0 +1,31 @@
+﻿namespace MrTechLead;
+
+public static class Console
+{
+    public static void WriteLine(string message)
+    {
+        System.Console.WriteLine(message);
+    }
+
+    public static void WriteLine(string message, System.ConsoleColor foregroundColor)
+    {
+        var currentForegroundColor = System.Console.ForegroundColor;
+        System.Console.ForegroundColor = foregroundColor;
+        System.Console.WriteLine(message);
+        System.Console.ForegroundColor = currentForegroundColor;
+    }
+
+    public static void WriteLine(string message, System.ConsoleColor foregroundColor, System.ConsoleColor backgroundColor)
+    {
+        var currentForegroundColor = System.Console.ForegroundColor;
+        var currentBackgroundColor = System.Console.BackgroundColor;
+
+        System.Console.ForegroundColor = foregroundColor;
+        System.Console.BackgroundColor = backgroundColor;
+
+        System.Console.WriteLine(message);
+
+        System.Console.ForegroundColor = currentForegroundColor;
+        System.Console.BackgroundColor = currentBackgroundColor;
+    }
+}
