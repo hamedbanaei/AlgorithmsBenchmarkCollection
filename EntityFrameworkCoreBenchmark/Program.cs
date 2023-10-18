c6b0c69f49dc29055dc8f1e4ff0366e001f13dad
Hamed Banaei <hamedbanaei1982@gmail.com>
2023-10-18 15:24:02 +0330

    WhereAndSelectPriority Benchmark gets the same performance and deleted.

--- EntityFrameworkCoreBenchmark/Program.cs
+++ EntityFrameworkCoreBenchmark/Program.cs
@@ -1,5 +1,4 @@
 ﻿//var summary = BenchmarkDotNet.Running.BenchmarkRunner.Run<Benchmarks.Linq.SkipTakeVsTakeRange>();
-//var summary = BenchmarkDotNet.Running.BenchmarkRunner.Run<Benchmarks.WhereAndSelectPriority>();
 //var summary = BenchmarkDotNet.Running.BenchmarkRunner.Run<Benchmarks.Linq.ImprovePerformanceWithAsNoTracking>();
 //var summary = BenchmarkDotNet.Running.BenchmarkRunner.Run<Benchmarks.TraditionalUpdateVsExecuteUpdate>();
 //var summary = BenchmarkDotNet.Running.BenchmarkRunner.Run<Benchmarks.TraditionalDeleteVsExecuteDelete>();
