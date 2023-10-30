40752f0b2e10f797c9130ff3b6adfb531608f69d
Hamed Banaei <hamedbanaei1982@gmail.com>
2023-10-30 16:50:54 +0330

    PreferAnyOverCountForExistenceCheck benchmark done.

---
+++ EntityFrameworkCoreBenchmark/Benchmarks/Linq/PreferAnyOverCountForExistenceCheck.cs
@@ -0,0 +1,35 @@
+﻿namespace Benchmarks.Linq;
+
+[BenchmarkDotNet.Attributes.MemoryDiagnoser]
+[BenchmarkDotNet.Attributes.Orderer(BenchmarkDotNet.Order.SummaryOrderPolicy.FastestToSlowest)]
+public class PreferAnyOverCountForExistenceCheck
+{
+	public Model.DatabaseContext DbContext { get; set; }
+
+	[BenchmarkDotNet.Attributes.GlobalSetup]
+	public void GlobalSetup()
+	{
+		Benchmarks.GlobalAndIterationSetup.FillDatabaseWithFakeTestData
+			(testFatherCount: 2_000_000, ignoreCheckIfDatabaseHasData: false);
+
+		DbContext = new Model.DatabaseContext();
+	}
+
+	[BenchmarkDotNet.Attributes.Benchmark]
+	public bool ExistenceCheckUsingCount()
+	{
+		bool blnResult = DbContext.Children
+			.Count(child => child.Name == "John") > 0;
+
+		return (blnResult);
+	}
+
+	[BenchmarkDotNet.Attributes.Benchmark]
+	public bool ExistenceCheckUsingAny()
+	{
+		bool blnResult = DbContext.Children
+			.Any(child => child.Name == "John");
+
+		return (blnResult);
+	}
+}
--- EntityFrameworkCoreBenchmark/Program.cs
+++ EntityFrameworkCoreBenchmark/Program.cs
@@ -2,4 +2,5 @@
 //var summary = BenchmarkDotNet.Running.BenchmarkRunner.Run<Benchmarks.Linq.ImprovePerformanceWithAsNoTracking>();
 //var summary = BenchmarkDotNet.Running.BenchmarkRunner.Run<Benchmarks.TraditionalUpdateVsExecuteUpdate>();
 //var summary = BenchmarkDotNet.Running.BenchmarkRunner.Run<Benchmarks.TraditionalDeleteVsExecuteDelete>();
-var summary = BenchmarkDotNet.Running.BenchmarkRunner.Run<Benchmarks.Linq.ImprovePerformanceWithProjection>();
+//var summary = BenchmarkDotNet.Running.BenchmarkRunner.Run<Benchmarks.Linq.ImprovePerformanceWithProjection>();
+var summary = BenchmarkDotNet.Running.BenchmarkRunner.Run<Benchmarks.Linq.PreferAnyOverCountForExistenceCheck>();
